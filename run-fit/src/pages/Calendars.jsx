539958cd3c6dc2af814283228c77bf9174362964
Chad Tarpey <chadtarpey@gmail.com>
2022-07-26 20:39:01 -0500

    created calendars login signup events users pages

---
+++ run-fit/src/pages/Calendars.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Calendars = () => {
+    return <div>Calendars coming soon</div>;
+};
+
+export default Calendars;
---
+++ run-fit/src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+    return <div>SignUp coming soon</div>;
+};
+
+export default SignUp;
